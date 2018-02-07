cd44e36d3044eb1b4e6661b5d3a59d08b608b220
P-Archana <36073251+P-Archana@users.noreply.github.com>
2018-02-07 12:30:54 +0530

    Delete prime_range,py.py

--- prime_range,py.py
+++
@@ -1,9 +0,0 @@
-l=input("enter the lower limit")
-u=input("enter the upper limit")
-for num in range(l,u + 1):
-   if num > 1:
-       for i in range(2,num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
