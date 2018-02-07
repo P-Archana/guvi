9daa037ba7a5a4122d2596c0d9cd72f34d2cba44
P-Archana <36073251+P-Archana@users.noreply.github.com>
2018-02-07 07:33:57 +0530

    Add files via upload

---
+++ leap_6.py
@@ -0,0 +1,11 @@
+year=int(raw_input("enter the year"))
+if(year%4)==0:
+    if(year%100)==0:
+        if(year%400)==0:
+            print' it is a leap year'
+        else:
+            print'not leap year'
+    else:
+        print'leap year'
+else:
+    print'not leap year'
