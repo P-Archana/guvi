50a17c36438ffe3b8399aadd010fd24076629c00
P-Archana <36073251+P-Archana@users.noreply.github.com>
2018-02-07 14:55:45 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,9 +1,10 @@
 num=input("enter the no")
 sum=0
+l=len(str(num))
 temp = num
 while temp > 0:
    digit = temp % 10
-   sum += digit ** 3
+   sum += digit ** l
    temp /= 10
 if num == sum:
    print str(num)+" is an Armstrong number"
